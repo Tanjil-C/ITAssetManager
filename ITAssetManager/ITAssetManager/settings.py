f563e9970faa05d31ddaf120048c0c6c881d9198
Tanjil <tanjil.ahmed-choudhury@mrisoftware.com>
2024-09-20 19:53:45 +0100

    url file fix

--- ITAssetManager/ITAssetManager/settings.py
+++ ITAssetManager/ITAssetManager/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ITAssetManager.urls'
+ROOT_URLCONF = 'ITAssetManager.ITAssetManager.urls'
 
 # Template configuration
 TEMPLATES = [
