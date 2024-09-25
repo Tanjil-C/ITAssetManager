8c8191369df9849b9d20cc4001bb588dcd9d77af
Tanjil <tanjil.ahmed-choudhury@mrisoftware.com>
2024-09-25 23:53:11 +0100

    reduced registration redirect time

--- ITAssetManager/app/static/app/scripts/register.js
+++ ITAssetManager/app/static/app/scripts/register.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 // Redirect after 3 seconds
                 setTimeout(function () {
                     window.location.href = '/login';
-                }, 20000);
+                }, 6000);
             } else {
                 displayErrors(result.errors); // Display errors
             }
